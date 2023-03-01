fbd00a92af99827b22db091f53d95765b5009828
Chris Kjellqvist <chriskjellqvist@gmail.com>
2023-03-01 14:10:13 -0500

    use non-huge pages for small allocations.

--- src/fpga_handle.cc
+++ src/fpga_handle.cc
@@ -66,24 +66,24 @@ using namespace composer;
 
 const int __endian_bit = 1;
 #define is_bigendian() ( (*(char*)&__endian_bit) == 0 )
-#define PAGEMAP_ENTRY 8
-#define GET_BIT(X,Y) (X & ((uint64_t)1<<Y)) >> Y
-#define GET_PFN(X) X & 0x7FFFFFFFFFFFFF
+#define PAGEMAP_ENTRY (8)
+#define GET_BIT(X,Y) ((X & ((uint64_t)1<<Y)) >> Y)
+#define GET_PFN(X) (X & 0x7FFFFFFFFFFFFF)
 
 uint64_t vtop(unsigned long virt_addr) {
-  printf("Big endian? %d\n", is_bigendian());
+//  printf("Big endian? %d\n", is_bigendian());
   FILE *f = fopen("/proc/self/pagemap", "rb");
   if (!f) {
-    printf("Error! Cannot open /proc/self/pagemap");
-    return -1;
+    std::cerr << "Error! Cannot open /proc/self/pagemap" << std::endl;
+    throw std::exception();
   }
 
   //Shifting by virt-addr-offset number of bytes
   //and multiplying by the size of an address (the size of an entry in pagemap file)
-  long file_offset = virt_addr / getpagesize() * PAGEMAP_ENTRY;
+  unsigned long file_offset = virt_addr / getpagesize() * PAGEMAP_ENTRY;
   printf("Vaddr: 0x%lx, Page_size: %d, Entry_size: %d\n", virt_addr, getpagesize(), PAGEMAP_ENTRY);
   printf("Reading pagemap at 0x%llx\n", (unsigned long long) file_offset);
-  int status = fseek(f, file_offset, SEEK_SET);
+  int status = fseek(f, (long)file_offset, SEEK_SET);
   if (status) {
     perror("Failed to do fseek!");
     return -1;
@@ -92,31 +92,30 @@ uint64_t vtop(unsigned long virt_addr) {
   uint64_t read_val = 0;
   unsigned char c_buf[PAGEMAP_ENTRY];
   for (int i = 0; i < PAGEMAP_ENTRY; i++) {
-    char c = getc(f);
+    int c = getc(f);
     if (c == EOF) {
       printf("\nReached end of the file\n");
       return 0;
     }
     if (is_bigendian())
-      c_buf[i] = c;
+      c_buf[i] = (char)c;
     else
-      c_buf[PAGEMAP_ENTRY - i - 1] = c;
+      c_buf[PAGEMAP_ENTRY - i - 1] = (char)c;
     printf("[%d]0x%x ", i, c);
   }
-  for (int i = 0; i < PAGEMAP_ENTRY; i++) {
+  for (unsigned char i : c_buf) {
     //printf("%d ",c_buf[i]);
-    read_val = (read_val << 8) + c_buf[i];
+    read_val = (read_val << 8) + i;
   }
-  printf("\n");
-  printf("Result: 0x%llx\n", (unsigned long long) read_val);
-  //if(GET_BIT(read_val, 63))
-  uint64_t pfn;
-  if (GET_BIT(read_val, 63))
-    printf("PFN: 0x%llx\n", pfn = (unsigned long long) GET_PFN(read_val));
-  else
-    printf("Page not present\n");
-  fclose(f);
-  return pfn;
+
+  if (GET_BIT(read_val, 63)){
+    fclose(f);
+    return GET_PFN(read_val);
+  } else {
+    std::cerr << "Paged not mapped in!" << std::endl;
+    fclose(f);
+    throw std::exception();
+  }
 }
 
 #endif
@@ -222,9 +221,9 @@ remote_ptr fpga_handle_t::malloc(size_t len) {
   void *addr;
   size_t sz;
   if (len <= 1 << 12) {
+    sz = 1 << 12;
     addr = mmap(nullptr, sz, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_LOCKED | MAP_ANONYMOUS,
                 -1, 0);
-    sz = 1 << 12;
   } else {
     // Kria only uses local mappings via OS
     // see if allocation fits inside size classes
