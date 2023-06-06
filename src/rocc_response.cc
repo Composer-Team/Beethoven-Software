c48be6354356b5ca7061b208bfade79177612437
brendanpeercy <nbabsp@gmail.com>
2023-06-06 11:09:00 -0400

    fixed rocc_response bug

--- src/rocc_response.cc
+++ src/rocc_response.cc
@@ -9,5 +9,5 @@ rocc_response::rocc_response(const uint32_t *buffer, const composer_pack_info &p
   this->rd = (uint8_t) buffer[2] & 0x1F;
   this->system_id = (buffer[2] >> 5) & ((1 << pack_info.system_id_bits) - 1);
   this->core_id = (buffer[2] >> (5 + pack_info.system_id_bits)) & ((1 << pack_info.core_id_bits) - 1);
-  this->data = (uint64_t)(buffer[0]) | buffer[1];
+  this->data = (uint64_t)(buffer[0]) | ((uint64_t)buffer[1] << 32);
 }
