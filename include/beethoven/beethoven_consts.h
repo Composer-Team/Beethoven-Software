0edde56bb18dda3c9cc5e9220e1f2649ae25f949
Chris Kjellqvist <chriskjellqvist@gmail.com>
2024-08-06 15:45:20 -0400

    add back in flush so that we can communicate with server

--- include/beethoven/beethoven_consts.h
+++ include/beethoven/beethoven_consts.h
@@ -2,5 +2,6 @@
 #define BEETHOVEN_CONSTS_H
 
 #define ROCC_OP_ACCEL 0x7b
+#define ROCC_OP_FLUSH 0xb
 
 #endif
--- src/verilator_server.cc
+++ src/verilator_server.cc
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
 
 using namespace beethoven;
 
