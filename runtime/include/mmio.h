09b199a9b028cc02586809d2b9e863bba983977f
Mason Ma <maao666@icloud.com>
2025-10-07 22:46:18 +0000

    mmio fix for different bus widths

--- runtime/include/mmio.h
+++ runtime/include/mmio.h
@@ -8,11 +8,24 @@
 #include <beethoven_hardware.h>
 #include <cinttypes>
 
+#ifdef Kria
+#if AXIL_BUS_WIDTH == 64
+using devmem_ptr_t = uint64_t;
+#elif AXIL_BUS_WIDTH == 32
+using devmem_ptr_t = uint32_t;
+#else
+using devmem_ptr_t = uint8_t;
+#endif
+using peek_t = devmem_ptr_t;
+extern devmem_ptr_t *devmem_map;
+#else
+using peek_t = uint32_t;
+#endif
 
 void setup_mmio();
 
-void poke_mmio(uint64_t addr, uint32_t val);
 
-uint32_t peek_mmio(uint32_t addr);
+void poke_mmio(uint64_t off, uint32_t val);
+peek_t peek_mmio(uint32_t addr);
 
 #endif //BEETHOVEN_VERILATOR_MMIO_H
